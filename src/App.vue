133a54f7dc4fd80bd9b6f054f22398f3ed16313d
lipeng <lipeng@daokoudai.com>
2019-09-09 16:02:04 +0800

    8.Vue-路由Router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template lang='pug'>
   #id
     //- AddBlog
-    ShowBlogs
+    //- ShowBlogs
+    BlogHeader
+    router-view
 </template>
 
 <script lang="ts">
 import AddBlog from "./components/AddBlog.vue";
 import ShowBlogs from "./components/ShowBlogs.vue";
+import BlogHeader from "./components/BlogHeader.vue";
 
 import {Component,Vue,} from 'vue-property-decorator'
 @Component({
-  components:{AddBlog,ShowBlogs}
+  components:{AddBlog,ShowBlogs,BlogHeader,}
 })
 export default class ViewComponent extends Vue {
 
 }
 </script>
+
---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,38 @@
+<template lang='pug'>
+  nav
+    ul
+      li
+        router-link(to='/' exact) 博客
+        router-link(to='/add' exact) 写博客
+</template>
+
+<script lang="ts">import {Component,Vue,} from 'vue-property-decorator'
+@Component({
+  components:{}
+})
+export default class ViewComponent extends Vue {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+nav
+  background-color crimson 
+  padding 30px 0
+  margin-bottom 40px
+  ul
+    list-style-type none 
+    text-align center
+    margin 0
+    li
+      display inline-block
+      margin 0 10px
+a
+  color #ffffff
+  text-decoration  none
+  padding 12px
+  border-radius 5px
+.router-link-active
+  background-color rgba(255,255,255,.8)
+  color #444
+</style>
