e45b810b1dac7428e58ebe43d90167e78765d15b
lipeng <lipeng@daokoudai.com>
2019-09-09 20:17:16 +0800

    11.Vue-get拉取数据及项目打包上传到服务器v0.4.1:线上生产环境：生成sourceMap文件，调试资源加载问题

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const prodConfig = process.env.NODE_ENV === 'production' 
   ? {
     publicPath: './', // 线上相对路径调整
-    productionSourceMap: false, // 生产环境map(同configureWebpack下devtool)
+    // productionSourceMap: false, // 生产环境map(同configureWebpack下devtool)
   }
   : {}
 
