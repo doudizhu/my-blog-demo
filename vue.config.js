21c2c9569c8713a6c3c42ef60e677946e3da1384
lipeng <lipeng@daokoudai.com>
2019-09-09 19:44:46 +0800

    11.Vue-get拉取数据及项目打包上传到服务器v0.2:线上生产环境编译配置

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const prodConfig = process.env.NODE_ENV === 'production' 
+  ? {
+    publicPath: './', // 线上相对路径调整
+    productionSourceMap: false, // 生产环境map(同configureWebpack下devtool)
+  }
+  : {}
+
+module.exports = {
+  ...prodConfig,
+}
