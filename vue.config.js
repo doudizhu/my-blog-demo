882cc2ff2677c87a46906f8cf52c70a7eb9bda28
lipeng <lipeng@daokoudai.com>
2019-09-09 20:23:38 +0800

    11.Vue-get拉取数据及项目打包上传到服务器v0.4.2:线上生产环境：github预览时：相对路径，深入切换页面时，加载资源文件路径会有缺失问题

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const prodConfig = process.env.NODE_ENV === 'production' 
   ? {
-    publicPath: './', // 线上相对路径调整
-    // productionSourceMap: false, // 生产环境map(同configureWebpack下devtool)
+    // publicPath: './', // 线上相对路径调整
+    publicPath: 'https://doudizhu.github.io/my-blog-demo/dist/', // github预览时：相对路径，深入切换页面时，加载资源文件路径会有缺失问题
+    productionSourceMap: false, // 生产环境map(同configureWebpack下devtool)
   }
   : {}
 
