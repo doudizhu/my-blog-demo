599ec5a8ff4a1608bb9cf0541ff03a73ac467ec2
lipeng <lipeng@daokoudai.com>
2019-09-10 14:01:37 +0800

    13.1 markdown:安装element-ui

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import VueResource from "vue-resource";
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 Vue.use(VueResource)
 
